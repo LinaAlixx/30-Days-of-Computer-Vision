8c26cc3569171828067b0226d7fb0e073b185aaa
Lina Ali Bashawyah <123448420+LinaAlixx@users.noreply.github.com>
2026-08-01 23:39:11 +0300

    Update RW_vid.py

--- Day_01/RW_vid.py
+++ Day_01/RW_vid.py
@@ -1,1 +1,17 @@
+import cv2
 
+#Read Video
+vid = cv2.VideoCapture(r"videu path")
+
+#Visiualize Video
+ret = True
+while ret:
+    ret, frame = vid.read()
+
+    if ret:
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(40)
+
+vid.release()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
