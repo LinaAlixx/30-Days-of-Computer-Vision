2cff38053b4614d844021940043abe6bbf377cbc
Lina Ali Bashawyah <123448420+LinaAlixx@users.noreply.github.com>
2026-08-01 19:12:33 +0300

    Update RW_img.py

--- Day_01/RW_img.py
+++ Day_01/RW_img.py
@@ -1,1 +1,16 @@
+import cv2
+import os
 
+#Read Image
+img_path = os.path.join('..','E','images.jpg')
+img = cv2.imread(img_path)
+
+#Write Image
+cv2.imwrite(os.path.join('..','30 CV', 'image2.jpg'), img)
+
+
+#Visiualize Image
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
